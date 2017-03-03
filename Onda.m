410d5d319b95e9e2b58f1721af5449d09f634b6e
emirojaseng <emirojaseng@gmail.com>
2017-03-02 18:19:49 -0600

    Add files via upload

--- Onda.m
+++ Onda.m
@@ -1,6 +1,5 @@
 function Onda(xmin, xmax, t0, T, h, k, c, U0, UT)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+%Resuelve numéricamente la ecuación de la onda.
 
 %crea la malla
 x=xmin:h:xmax;
@@ -18,37 +17,37 @@ end
 %Este es el primer plot inicial
 plot(x,Uantes);
 pause(3.*k);
-
-%Éste es el primer tiempo i.e U(x,t1
+%Éste es el primer tiempo i.e U(x,t1)
 for i=1:size(x,2)
-    Ucero(i)=UT(x(i));
+    Ucero(i)=Uantes(i);
 end
 %segundo plot inicial
 plot(x,Ucero);
 pause(3.*k);
-
 %Ahora si empieza el método
 for j=1:size(t,2)
     %Calcula los valores en t+1 con diferencias finitas
     %Es en éste paso donde se usa el método
-    for i=1:(size(x,2)) %Toma en cuenta la "malla" que utiliza
-     %   if i==1
-      %    Unuevo(i)=2*Ucero(i)-Uantes(i)+(lambda.^2)*c.^2*(Ucero(i+1)-2*Ucero(i)+Ucero(size(x,2)))
-       % else if i==size(x,2)
-        %        Unuevo(i)=2*Ucero(i)-Uantes(i)+(lambda.^2)*c.^2*(Ucero(1)-2*Ucero(i)+Ucero(i-1))
-         %   else
-    Unuevo(i)=2*Ucero(i)-Uantes(i)
-    +(lambda.^2)*c.^2*(Ucero(mod(i+1,size(x,2)))-2*Ucero(i)+Ucero(mod(i-1),size(x,2)));
-        %    end
-       % end
-    end
-    disp('listo')
+    for i=1:(size(x,2))
+     if i==1
+      Unuevo(i)=2*Ucero(i)-Uantes(i)+(lambda.^2)*c.^2*(Ucero(i+1)-2*Ucero(i)+Ucero(size(x,2)));
+        else if i==size(x,2)
+        Unuevo(i)=2*Ucero(i)-Uantes(i)+(lambda.^2)*c.^2*(Ucero(1)-2*Ucero(i)+Ucero(i-1));
+            else
+                    %El importante
+                    Unuevo(i)=2*Ucero(i)-Uantes(i)+(lambda.^2)*(c.^2)*(Ucero(i+1)-2*Ucero(i)+Ucero(i-1));
+                end
+     end
+     end
+    
     %hace el plot de lo que recién calculamos
-    plot(x,Unuevo);
+    plot(x,Unuevo, '-s');
+    axis([-2, 2, -2, 2])
     pause(3.*k);
+    %waitforbuttonpress
     %Recorre los vectores
         for i=1:size(x,2)
-            Uviejo(i)=Ucero(i);
+            Uantes(i)=Ucero(i);
             Ucero(i)=Unuevo(i);
         end
 end
